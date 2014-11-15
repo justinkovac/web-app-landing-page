cfdac72a0b7522d7cfd19f569e8e8c7dd4504f2d
justinkovac <justin@pargee.com>
2014-11-14 22:54:32 -0600

    Initial commit
    Pushing initial content.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+//    $(".title-caption").css('opacity', '0' );
+//    $( ".title" ).effect( 'bounce',  750, function() {
+//        $(".title-caption").fadeTo( 500, 1 );
+//    });
+    
+});
